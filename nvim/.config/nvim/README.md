576d8a368cb72924f252116381d3d28e0990f94d
Matheus Elis <matheus.elis.silva@gmail.com>
2025-11-19 17:26:52 -0300

    include initial dotfiles

---
+++ nvim/.config/nvim/README.md
@@ -0,0 +1,34 @@
+# COMUVIM
+
+
+
+## Features
+- File explorer with [Neo-tree](https://github.com/nvim-neo-tree/neo-tree.nvim)
+- Autocompletion with [Cmp](https://github.com/hrsh7th/nvim-cmp)
+- Git integration with [Gitsigns](https://github.com/lewis6991/gitsigns.nvim) and [LazyGit](https://github.com/jesseduffield/lazygit)
+- Fuzzy finding with [Telescope](https://github.com/nvim-telescope/telescope.nvim)
+- Syntax highlighting with [Treesiter](https://github.com/nvim-treesitter/nvim-treesitter)
+- Language Server Protocol with [Native LSP](https://github.com/neovim/nvim-lspconfig)
+- Buffer Line with [Bufferline.nvim](https://github.com/akinsho/bufferline.nvim)
+
+
+## Installation
+
+### Dependencies
+
+- fd[https://github.com/sharkdp/fd]
+- ripgrep[https://github.com/BurntSushi/ripgrep]
+- treesitter-cli[https://www.npmjs.com/package/tree-sitter-cli]
+
+Make a backup of your current nvim and shared folder
+
+```
+mv ~/.config/nvim ~/.config/nvim.bak
+mv ~/.local/share/nvim ~/.local/share/nvim.bak
+```
+
+Clone the repository
+```
+git clone https://github.com/MatheusElis/ComuVim ~/.config/nvim
+nvim
+```
---
+++ nvim/.config/nvim/lua/comuvim/core/keymaps.lua
@@ -0,0 +1,33 @@
+local function map(m, k, v, d)
+  vim.keymap.set(m, k, v, { silent = true, desc = d })
+end
+
+-- Normal Keymaps --
+
+map("n", "|", "<cmd>vsplit<cr>", "Vertical Split")
+map("n", "\\", "<cmd>split<cr>", "Horizontal Split")
+
+-- Manage Buffers
+map("n", "<leader>bn", "<CMD>enew<CR>", "New File")
+map("n", "<leader>bc", "<cmd>bdelete<CR>", "Close buffer!")
+map("n", "<leader>bC", "<cmd>bdelete!<CR>", "Force close buffer!")
+map("n", "<S-Tab>", ":BufferLineCycleNext<CR>", "Next Buffer")
+map("n", "<S-l>", "<cmd>BufferLineCycleNext<cr>", "Next buffer tab")
+map("n", "<S-h>", "<cmd>BufferLineCyclePrev<cr>", "Previous buffer tab")
+map("n", ">b", "<cmd>BufferLineMoveNext<cr>", "Move buffer tab right")
+map("n", "<b", "<cmd>BufferLineMovePrev<cr>", "Move buffer tab left")
+
+-- Stay in indent mode
+map("v", "<", "<gv", "unindent line")
+map("v", ">", ">gv", "indent line")
+
+-- Navigation
+map("n", "<C-h>", "<C-w><C-h>", "Move focus to the left window")
+map("n", "<C-l>", "<C-w><C-l>", "Move focus to the right window")
+map("n", "<C-j>", "<C-w><C-j>", "Move focus to the lower window")
+map("n", "<C-k>", "<C-w><C-k>", "Move focus to the upper window")
+
+map("x", "<leader>p", [["_dP"]], "Paste without change de buffer")
+
+map({ "n", "v" }, "<leader>y", [["+y]], "Copy to the clipboard")
+map({ "n", "v" }, "<leader>d", '"_d', "Delete to the void")
